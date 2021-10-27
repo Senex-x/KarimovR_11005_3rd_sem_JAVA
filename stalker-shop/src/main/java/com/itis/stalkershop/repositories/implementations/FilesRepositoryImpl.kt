13ee175d50e48095f9d2e59bf0b79ce399f6fbb5
V-Senex <junior.07.inator@gmail.com>
2021-10-27 19:11:31 +0300

    Add repositories for models

---
+++ stalker-shop/src/main/java/com/itis/stalkershop/repositories/implementations/FilesRepositoryImpl.kt
@@ -0,0 +1,99 @@
+package com.itis.stalkershop.repositories.implementations
+
+import com.itis.stalkershop.models.UploadedFile
+import com.itis.stalkershop.models.UploadedFileDto
+import com.itis.stalkershop.repositories.interfaces.FilesRepository
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
+import org.springframework.jdbc.support.GeneratedKeyHolder
+import java.sql.Connection
+import java.sql.ResultSet
+import java.util.*
+import javax.sql.DataSource
+
+private const val SQL_INSERT =
+    "insert into file_info(storage_file_name, original_file_name, type, size) values (?, ?, ?, ?)"
+private const val SQL_UPDATE =
+    "update file_info set storage_file_name = ?, original_file_name = ?, type = ?, size = ? where id = ?"
+private const val SQL_SELECT_BY_ID =
+    "select * from file_info where id = ?"
+
+class FilesRepositoryImpl(dataSource: DataSource) : FilesRepository {
+    private val jdbcTemplate: JdbcTemplate
+
+    init {
+        jdbcTemplate = JdbcTemplate(dataSource)
+    }
+
+    private val fileRowMapper =
+        RowMapper { row: ResultSet, _ ->
+            row.run {
+                UploadedFile(
+                    getLong("id"),
+                    getString("original_file_name"),
+                    getString("storage_file_name"),
+                    getLong("size"),
+                    getString("type")
+                )
+            }
+        }
+
+
+    // TODO: handle save or update cases
+    override fun save(item: UploadedFileDto): UploadedFile {
+        return if (true) { // if not present
+            val keyHolder = GeneratedKeyHolder()
+            jdbcTemplate.update({ connection: Connection ->
+                connection.prepareStatement(SQL_INSERT, arrayOf("id")).apply {
+                    setString(1, item.storageFileName)
+                    setString(2, item.originalFileName)
+                    setString(3, item.type)
+                    setLong(4, item.size)
+                }
+            }, keyHolder)
+
+            item.toUploadedFile(keyHolder.key!!.toLong())
+        } else { // if present (add get id from database logic)
+            /*
+            item.apply {
+                jdbcTemplate.update(
+                    SQL_UPDATE,
+                    storageFileName,
+                    originalFileName,
+                    type,
+                    size,
+                    id
+                )
+            }.toUploadedFile(id)*/
+            TODO("Not yet implemented")
+        }
+    }
+
+    override fun update(primaryKey: Long, item: UploadedFileDto) {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun findByPrimaryKey(id: Long): Optional<UploadedFile> {
+        return try {
+            Optional.ofNullable(
+                jdbcTemplate.queryForObject(
+                    SQL_SELECT_BY_ID,
+                    fileRowMapper,
+                    id
+                )
+            )
+        } catch (e: EmptyResultDataAccessException) {
+            Optional.empty()
+        }
+    }
+
+    // TODO: Реализовать
+    override fun findAll(): List<UploadedFile> {
+        return emptyList()
+    }
+
+    // TODO: Реализовать
+    override fun delete(primaryKey: Long) {}
+}
---
+++ stalker-shop/src/main/java/com/itis/stalkershop/repositories/implementations/UsersRepositoryImpl.java
@@ -0,0 +1,125 @@
+package com.itis.stalkershop.repositories.implementations;
+
+
+import com.itis.stalkershop.models.User;
+import com.itis.stalkershop.models.UserDto;
+import com.itis.stalkershop.repositories.interfaces.UsersRepository;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.Types;
+import java.util.List;
+import java.util.Optional;
+
+public class UsersRepositoryImpl implements UsersRepository {
+    private static final String SQL_INSERT =
+            "insert into users(email, name, password_hash, avatar_id) values (?, ?, ?, ?)";
+    private static final String SQL_UPDATE =
+            "update users set email = ?, name = ?, avatar_id = ? where email = ?";
+    private static final String SQL_SELECT_BY_EMAIL =
+            "select * from users where email = ?";
+    private static final String SQL_SELECT_ALL =
+            "select * from users";
+
+    private final RowMapper<User> rowMapper = (row, rowNumber) ->
+            new User(
+                    row.getString("email"),
+                    row.getString("name"),
+                    row.getString("password_hash"),
+                    row.getLong("avatar_id")
+            );
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UsersRepositoryImpl(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @NotNull
+    @Override
+    public Optional<User> findByEmail(@NotNull String email) {
+        try {
+            return Optional.ofNullable(
+                    jdbcTemplate.queryForObject(SQL_SELECT_BY_EMAIL, rowMapper, email)
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<User> findByPrimaryKey(String id) {
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject(SQL_SELECT_BY_EMAIL, rowMapper, id));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<User> findAll() {
+        return jdbcTemplate.query(SQL_SELECT_ALL, rowMapper);
+    }
+
+    // TODO: Handle save or update
+    //  according to user's data presence in database
+    @Override
+    public User save(UserDto item) {
+        if (true) {
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+            jdbcTemplate.update(connection -> {
+                PreparedStatement statement = connection.prepareStatement(SQL_INSERT, new String[]{"id"});
+                statement.setString(1, item.getEmail());
+                statement.setString(2, item.getName());
+                statement.setString(3, item.getPasswordHash());
+                if (item.getAvatarId() != null) {
+                    statement.setLong(4, item.getAvatarId());
+                } else {
+                    statement.setNull(4, Types.NULL);
+                }
+                return statement;
+            }, keyHolder);
+
+            return item.toUser();
+        } else {
+            // Not safe operation
+            jdbcTemplate.update(SQL_UPDATE,
+                    item.getEmail(),
+                    item.getName(),
+                    item.getAvatarId(),
+                    item.getEmail()
+            );
+        }
+        return item.toUser();
+    }
+
+    // TODO: Implement
+    @Override
+    public void update(String primaryKey, UserDto item) {
+        jdbcTemplate.update(connection -> {
+                    PreparedStatement statement = connection.prepareStatement(SQL_UPDATE, new String[]{"id"});
+                    statement.setString(1, item.getEmail());
+                    statement.setString(2, item.getName());
+                    if (item.getAvatarId() != null) {
+                        statement.setLong(3, item.getAvatarId());
+                    } else {
+                        statement.setNull(3, Types.NULL);
+                    }
+                    statement.setString(4, item.getEmail());
+                    return statement;
+                },
+                new GeneratedKeyHolder()
+        );
+    }
+
+    // TODO: Implement
+    @Override
+    public void delete(String primaryKey) {
+    }
+}
