b2caec181d06a9e2c44cb2762fa17e264eea9847
V-Senex <junior.07.inator@gmail.com>
2021-10-28 23:41:48 +0300

    Add `CartServlet` and `ShopServlet` classes

---
+++ stalker-shop/src/main/java/com/itis/stalkershop/web/servlets/ShopServlet.java
@@ -0,0 +1,36 @@
+package com.itis.stalkershop.web.servlets;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ShopServlet extends HttpServlet {
+    @Override
+    public void init(
+            ServletConfig config
+    ) throws ServletException {
+        super.init(config);
+
+    }
+
+    @Override
+    protected void doGet(
+            HttpServletRequest req,
+            HttpServletResponse resp
+    ) throws ServletException, IOException {
+        super.doGet(req, resp);
+
+    }
+
+    @Override
+    protected void doPost(
+            HttpServletRequest req,
+            HttpServletResponse resp
+    ) throws ServletException, IOException {
+        super.doPost(req, resp);
+
+    }
+}
