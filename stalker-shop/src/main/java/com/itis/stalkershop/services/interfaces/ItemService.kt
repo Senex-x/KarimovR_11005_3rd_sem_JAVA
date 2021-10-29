b2cd09dec8ee7f8fe2b326e14aeba5942c0ffb22
V-Senex <junior.07.inator@gmail.com>
2021-10-29 20:06:00 +0300

    Add passing all items from database to `HttpServletRequest` in `CatalogServlet` `doGet()` method

--- stalker-shop/src/main/java/com/itis/stalkershop/services/interfaces/ItemService.kt
+++ stalker-shop/src/main/java/com/itis/stalkershop/services/interfaces/ItemService.kt
@@ -3,7 +3,9 @@ package com.itis.stalkershop.services.interfaces
 import com.itis.stalkershop.models.ItemDto
 
 interface ItemService {
-    fun getItemDto(
+    fun get(
         name: String
     ) : ItemDto
+
+    fun getAll() : List<ItemDto>
 }
