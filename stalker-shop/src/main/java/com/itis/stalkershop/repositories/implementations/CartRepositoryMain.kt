ffe3a4e5aaf9fc06d83240b2b9fa4f7c9f9c95a7
V-Senex <junior.07.inator@gmail.com>
2021-11-02 16:16:46 +0300

    Introduce `ReworkedRepository` with changed signatures

--- stalker-shop/src/main/java/com/itis/stalkershop/repositories/implementations/CartRepositoryMain.kt
+++ stalker-shop/src/main/java/com/itis/stalkershop/repositories/implementations/CartRepositoryMain.kt
@@ -2,18 +2,20 @@ package com.itis.stalkershop.repositories.implementations
 
 import com.itis.stalkershop.models.Cart
 import com.itis.stalkershop.repositories.interfaces.CartRepository
+import org.springframework.dao.DataAccessException
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.RowMapper
 import java.sql.ResultSet
 import java.util.*
 import javax.sql.DataSource
 
-private const val SQL_SELECT_BY_ID =
+private const val SQL_SELECT_BY_PRIMARY_KEY =
     "select * from carts where user_email = ?"
 private const val SQL_INSERT =
     "insert into carts(user_email, item_names_list_json) values (?, ?)"
 
-private val fileRowMapper =
+private val cartRowMapper =
     RowMapper { row: ResultSet, _ ->
         Cart(
             row.getString("user_email"),
@@ -24,23 +26,32 @@ private val fileRowMapper =
 class CartRepositoryMain(dataSource: DataSource) : CartRepository {
     private val jdbcTemplate: JdbcTemplate = JdbcTemplate(dataSource)
 
-    override fun findByPrimaryKey(primaryKey: String): Optional<Cart> {
+    override fun add(item: String) {
         TODO("Not yet implemented")
     }
 
-    override fun save(item: Cart): Cart {
+    override fun get(primaryKey: String) =
+        try {
+            jdbcTemplate.queryForObject(
+                SQL_SELECT_BY_PRIMARY_KEY,
+                cartRowMapper,
+                primaryKey
+            )
+        } catch (exception: DataAccessException) {
+            null
+        }
+
+    override fun getAll(): List<Cart> {
         TODO("Not yet implemented")
     }
 
-    override fun findAll(): List<Cart> {
-        TODO("Not yet implemented")
-    }
-
-    override fun update(primaryKey: String, item: Cart) {
+    override fun update(item: String) {
         TODO("Not yet implemented")
     }
 
     override fun delete(primaryKey: String) {
         TODO("Not yet implemented")
     }
+
+
 }
--- stalker-shop/src/main/java/com/itis/stalkershop/repositories/interfaces/CartRepository.kt
+++ stalker-shop/src/main/java/com/itis/stalkershop/repositories/interfaces/CartRepository.kt
@@ -2,4 +2,4 @@ package com.itis.stalkershop.repositories.interfaces
 
 import com.itis.stalkershop.models.Cart
 
-interface CartRepository: Repository<String, Cart, Cart>
+interface CartRepository: ReworkedRepository<String, Cart>
