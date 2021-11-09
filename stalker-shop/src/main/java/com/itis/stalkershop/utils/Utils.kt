7a371da335cf65fe9b511dbb285590744929638e
V-Senex <junior.07.inator@gmail.com>
2021-11-09 22:13:11 +0300

    Update `Utils` to simplify use

--- stalker-shop/src/main/java/com/itis/stalkershop/utils/Utils.kt
+++ stalker-shop/src/main/java/com/itis/stalkershop/utils/Utils.kt
@@ -2,17 +2,17 @@ package com.itis.stalkershop.utils
 
 import java.io.IOException
 import java.util.*
-import javax.servlet.ServletContext
+import javax.servlet.ServletConfig
 import javax.servlet.ServletContextListener
 
 // For use from Kotlin
 @JvmName("getAttributeKt")
-inline fun <reified T> ServletContext.getAttribute() =
-    getAttribute(simpleName<T>()) as T
+inline fun <reified T> ServletConfig.getAttribute() =
+    servletContext.getAttribute(simpleName<T>()) as T
 
 // For use from Java
-fun <T> getAttribute(type: Class<T>, context: ServletContext) =
-    context.getAttribute(type.simpleName) as T
+fun <T> getAttribute(type: Class<T>, config: ServletConfig) =
+     config.servletContext.getAttribute(type.simpleName) as T
 
 fun ServletContextListener.loadPropertiesFrom(fileName: String) = try {
     Properties().apply {
